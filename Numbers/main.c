a70803538b5c30a1a514ed1bb12ba3d6260516c8
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-10 16:04:25 -0700

    change number to unsigned long and add equation

--- Numbers/main.c
+++ Numbers/main.c
@@ -9,9 +9,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-  long x = 255;
-  printf("x is %ld.\n", x);
+  unsigned long x = 255;
+  printf("x is %lu.\n", x);
   printf("In octal, x is %lo.\n", x);
-  printf("In hexadecimal, x is %lx.\n,", x);
+  printf("In hexadecimal, x is %lx.\n", x);
+  
+  printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
   return 0;
 }
