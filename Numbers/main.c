84de1d0b544a6b31e0bf5ed74b025220a7fdd0d1
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-10 15:58:03 -0700

    print octal and hexidecimal numbers

--- Numbers/main.c
+++ Numbers/main.c
@@ -11,5 +11,7 @@
 int main(int argc, const char * argv[]) {
   int x = 255;
   printf("x is %d.\n", x);
+  printf("In octal, x is %o.\n", x);
+  printf("In hexadecimal, x is %x.\n,", x);
   return 0;
 }
