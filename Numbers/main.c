cd8ce9f0b2e39b124b3a8136a587ae0087b535d8
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-10 15:59:22 -0700

    change number to long

--- Numbers/main.c
+++ Numbers/main.c
@@ -9,9 +9,9 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-  int x = 255;
-  printf("x is %d.\n", x);
-  printf("In octal, x is %o.\n", x);
-  printf("In hexadecimal, x is %x.\n,", x);
+  long x = 255;
+  printf("x is %ld.\n", x);
+  printf("In octal, x is %lo.\n", x);
+  printf("In hexadecimal, x is %lx.\n,", x);
   return 0;
 }
